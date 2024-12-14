900cc74d8f911a6c81f81a7cf92b9db51f9a03bc
Rosbet75 <rosbet75@gmail.com>
2024-12-13 18:51:36 -0600

    script mysql finalizado

--- mysql/db.sql
+++ mysql/db.sql
@@ -11,17 +11,25 @@ create table usuarios(nickname varchar(50) primary key not null,
                     contra varchar(100) not null,
                     createdAt TIMESTAMP default CURRENT_TIMESTAMP,
                     updatedAt timestamp default current_timestamp on update CURRENT_TIMESTAMP);
-create table privilegios(idPriv int not null primary key auto_increment,
+create table privilegios(idPriv int primary key auto_increment,
                         rol varchar(30) not null,
                         createdAt TIMESTAMP default CURRENT_TIMESTAMP,
                         updatedAt timestamp default current_timestamp on update CURRENT_TIMESTAMP);
-create table cargoLaboral(idCargo varchar(50) not null,
-                        sueldoHora int not null);
+insert into privilegios values(null, "root", default, default);
+insert into privilegios values(null, "admin", default, default);
+create table cargoLaboral(idCargo int PRIMARY key AUTO_INCREMENT,
+                        cargo varchar(50) not null,
+                        sueldoHora int not null,
+                        createdAt TIMESTAMP default CURRENT_TIMESTAMP,
+                        updatedAt timestamp default current_timestamp on update CURRENT_TIMESTAMP);
+insert into cargoLaboral values(null, "Jefe de comunicacioens", 300, default, default);
+insert into cargoLaboral values(null, "Atencion de radio", 50, default, default);
+insert into cargoLaboral values(null, "Chofer", 150, default, default);
 create table empleados(curp varchar(18) not null primary key,
                     apellidoPaterno varchar(50) not null,
                     apellidoMaterno varchar(50) not null,
                     nombre varchar(50) not null,
-                    idCargo varchar(50) not null,
+                    idCargo int,
                     horarioEntrada time not null,
                     horarioSalida time not null,
                     createdAt TIMESTAMP default CURRENT_TIMESTAMP,
@@ -36,7 +44,7 @@ create table admins(nickname varchar(50) primary key not null,
                     createdAt TIMESTAMP default CURRENT_TIMESTAMP,
                     updatedAt timestamp default current_timestamp on update CURRENT_TIMESTAMP,
                     foreign key(curp) references empleados(curp) on delete cascade on update cascade,
-                    Foreign Key(privilegios) REFERENCES privilegios(idPriv) on delete set null on update cascade);
+                    Foreign Key(privilegios) REFERENCES privilegios(idPriv) on delete cascade on update cascade);
 create table vehiculos(idMatricula varchar(8) primary key not null,
                     anoVehiculo varchar(4) not null,
                     modelo varchar(20) not null,
@@ -45,23 +53,24 @@ create table vehiculos(idMatricula varchar(8) primary key not null,
                     disponible int not null,
                     createdAt TIMESTAMP default CURRENT_TIMESTAMP,
                     updatedAt timestamp default current_timestamp on update CURRENT_TIMESTAMP);
-create table choferes(idChofer int not null primary key auto_increment,
+
+create table choferes(idChofer int primary key auto_increment,
                     curp varchar(18) not null unique,
-                    num_licencia varchar(12) not null UNIQUE
-                    Foreign Key (curp) REFERENCES empleados(curp) on delete no action on update cascade);
-create table viajes(idViaje int not null primary key auto_increment,
-                idChofer varchar(18) not null,
+                    num_licencia varchar(12) not null UNIQUE,
+                    foreign Key(curp) references empleados(curp) on delete no action on update cascade);
+create table viajes(idViaje int primary key auto_increment,
+                idChofer int not null,
                 idUsuario varchar(50) not null,
                 destino varchar(50),
                 costo_viaje int not null,
                 cuota_ganancia int not null,
                 idMatricula varchar(8) not null,
-                foreign key idChofer references choferes(idChofer) on delete no action on update cascade,
-                foreign key idUsuario references usuarios(nickname) on delete no action on update cascade,
-                foreign key idMatricula references vehiculos(idMatricula) on delete no action on update CASCADE,
+                foreign key(idChofer) references choferes(idChofer) on delete RESTRICT on update cascade,
+                foreign key(idUsuario) references usuarios(nickname) on delete restrict on update cascade,
+                foreign key(idMatricula) references vehiculos(idMatricula) on delete restrict on update CASCADE,
                 createdAt TIMESTAMP default CURRENT_TIMESTAMP,
                 updatedAt timestamp default current_timestamp on update CURRENT_TIMESTAMP);
-create table queja(idQueja int not null primary key auto_increment,
+create table queja(idQueja int primary key auto_increment,
                 idViaje int not null,
                 comentarios varchar(500) not null,
                 atendido BOOLEAN not null,
@@ -69,7 +78,7 @@ create table queja(idQueja int not null primary key auto_increment,
                 createdAt TIMESTAMP default CURRENT_TIMESTAMP,
                 updatedAt timestamp default current_timestamp on update CURRENT_TIMESTAMP);
 
-create table reviews(idReview intnot null primary key auto_increment,
+create table reviews(idReview int primary key auto_increment,
                     idViaje int not null,
                     rating int not null,
                     comentarios varchar(500) not null,
@@ -78,8 +87,8 @@ create table reviews(idReview intnot null primary key auto_increment,
                     updatedAt timestamp default current_timestamp on update CURRENT_TIMESTAMP);
 create table tarjetas(numTar varchar(18) not null primary key,
                     fechaExp varchar(5) not null,
-                    cvv varchar(4) not null)
-create table pagos(idPago int not null primary key auto_increment,
+                    cvv varchar(4) not null);
+create table pagos(idPago int primary key auto_increment,
                     idTarjeta varchar(18) not null,
                     monto int not null,
                     idViaje int not null,
@@ -88,32 +97,32 @@ create table pagos(idPago int not null primary key auto_increment,
                     foreign key(idViaje) references viajes(idViaje) on delete cascade on update cascade,
                     createdAt TIMESTAMP default CURRENT_TIMESTAMP,
                     updatedAt timestamp default current_timestamp on update CURRENT_TIMESTAMP);
-create table conceptoCitas(idConcepto int not null primary key AUTO_INCREMENT,
+create table conceptoCitas(idConcepto int primary key AUTO_INCREMENT,
                             concepto varchar(100) not null unique,
                             createdAt TIMESTAMP default CURRENT_TIMESTAMP,
                             updatedAt timestamp default current_timestamp on update CURRENT_TIMESTAMP);
-create table conceptoSanciones(idSancion int not null primary key auto_increment,
+create table conceptoSanciones(idSancion int primary key auto_increment,
                                 sancion varchar(100) not null unique,
                                 createdAt TIMESTAMP default CURRENT_TIMESTAMP,
                                 updatedAt timestamp default current_timestamp on update CURRENT_TIMESTAMP);
-create table citas(idCita int not null primary key auto_increment,
+create table citas(idCita int primary key auto_increment,
                     idChofer int not null,
                     fechaCita datetime not null,
                     concepto int not null,
                     comentarios varchar(1000) not null,
                     sancion int,
-                    Foreign Key (idChofer) REFERENCES (choferes(idChofer)) on delete cascade on update cascade,
-                    foreign key concepto references conceptoCitas(idConcepto) on delete set null on update cascade,
-                    foreign key sancion references conceptoSanciones(idSancion) on delete set null on update cascade,
+                    foreign Key(idChofer) REFERENCES choferes(idChofer) on delete restrict on update cascade,
+                    foreign key (concepto) references conceptoCitas(idConcepto) on delete restrict on update cascade,
+                    foreign key (sancion) references conceptoSanciones(idSancion) on delete set null on update cascade,
                     createdAt TIMESTAMP default CURRENT_TIMESTAMP,
                     updatedAt timestamp default current_timestamp on update CURRENT_TIMESTAMP);
 
-create table atencionQueja(idAtencionQueja int not null primary key auto_increment,
+create table atencionQueja(idAtencionQueja int primary key auto_increment,
                             idQueja int not null,
                             comentarios varchar(1000) not null,
                             idCita int,
-                            foreign key idQueja references queja(idQueja) on delete cascade on update cascade,
-                            foreign key idCita references citas(idCita) on delete no action on update cascade,
+                            foreign key(idQueja) references queja(idQueja) on delete restrict on update cascade,
+                            foreign key(idCita) references citas(idCita) on delete restrict on update cascade,
                             createdAt TIMESTAMP default CURRENT_TIMESTAMP,
                             updatedAt timestamp default current_timestamp on update CURRENT_TIMESTAMP);
 
