808bb1a038263bb930899c42d0a7fc3d32a0428c
iranwav <esquivelagba64@gmail.com>
2024-12-19 18:08:22 -0600

    a

--- administradorAdmin.php
+++ administradorAdmin.php
@@ -159,7 +159,7 @@ if (!$result) {
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary color">
     <div class="container-fluid color">
-      <a class="navbar-brand white" href="#">Eneto.Inc</a>
+      <a class="navbar-brand white" href="BarraAdmin.php">Eneto.Inc</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
--- pagosAdmin.php
+++ pagosAdmin.php
@@ -264,7 +264,7 @@ if (isset($_POST['modificar'])) {
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary color">
     <div class="container-fluid color">
-      <a class="navbar-brand white" href="#">Eneto.Inc</a>
+      <a class="navbar-brand white" href="BarraAdmin.php">Eneto.Inc</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
